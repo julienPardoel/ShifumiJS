371e7ec8a06ced73e0e12e679181521112bfd66f
julien <julien.pardoel@gmail.com>
2022-12-08 05:10:12 +0100

    score

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const buttons = document.querySelectorAll("button");
-
+let score = 0;
+let scoreRobot = 0;
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
     const joueur = buttons[i].innerHTML;
     const robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
     let result = "";
+
+    // logique du jeu
     if (joueur === robot) {
       result = "Egalité !";
     } else if (
@@ -14,12 +17,16 @@ for (let i = 0; i < buttons.length; i++) {
       joueur === "Puit"
     ) {
       result = "Gagné";
+      score++;
+      console.log(score);
     } else {
       result = "Perdu";
+      scoreRobot++;
     }
-    console.log(`${joueur} vs ${robot} = ${result}`);
     document.querySelector(".response").innerHTML = `${result}`;
     document.querySelector(".you").innerHTML = `${joueur}`;
     document.querySelector(".robot").innerHTML = `${robot}`;
+    document.querySelector(".score-result").innerHTML = `${score}`;
+    document.querySelector(".score-robot").innerHTML = `${scoreRobot}`;
   });
 }
